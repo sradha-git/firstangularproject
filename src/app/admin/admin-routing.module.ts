8e27f32eba903a0116980378001a394855ee7eb4
basant807 <basant@syonindia.com>
2020-11-30 01:22:49 +0530

    fdgdffg

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { LoginComponent } from "./login/login.component";
 const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from './shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, LoginComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
