385dbe9045986a45101ed4c17b6a368de65d882e
sarada <sarada12ru@gmail.com>
2020-12-01 22:10:27 +0530

    Service Prepared

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor() { }
+
+  login(email:any, password:any){
+
+  }
+
+  add_member(email:any, password:any, role:any){
+
+  }
+
+  forgot_password(email:any){
+
+  }
+
+  add_catagory(data:any){
+
+  }
+
+  get_catagories(){
+
+  }
+
+  delete_catagory(id:any){
+
+  }
+
+  update_catagory(id:any, data:any){
+
+  }
+
+  add_products(data:AnalyserOptions){
+
+  }
+
+  get_products(){
+
+  }
+
+  delete_product(id:any){
+
+  }
+
+  update_product(id:any, data:any){
+
+  }
+
+}
