53d2477fc933dc4ba31ca02d6cfe286f8d51a5db
basant807 <basant@syonindia.com>
2020-12-02 02:57:55 +0530

    fdfgdffgdfg

--- src/app/admin/menus/menus.component.ts
+++ src/app/admin/menus/menus.component.ts
@@ -1,8 +1,12 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import $ from "jquery";
+
 declare var jquery: any;
 declare var $: any;
 declare var DataTable: any;
+
+
+const Swal = require('sweetalert2')
 @Component({
   selector: 'app-menus',
   templateUrl: './menus.component.html',
